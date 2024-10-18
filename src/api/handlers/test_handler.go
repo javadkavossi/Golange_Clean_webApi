daf37ba5825d05a57e507fee0bd675d18eaacd7b
javadkavossi <jkavossi@gmail.com>
2024-10-18 16:55:03 +0330

    add binder & form and & file & params & body

---
+++ src/api/handlers/test_handler.go
@@ -0,0 +1,138 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type header struct {
+	UserId  string
+	Browser string
+}
+
+type personData struct {
+	Name   string `json:"name"`
+	Family string `json:"family"`
+}
+
+type TestHandler struct {
+}
+
+func NewTestHandler() *TestHandler {
+	return &TestHandler{}
+}
+
+// =====================
+
+func (h *TestHandler) Test(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"result": "ok Test",
+	})
+
+}
+
+func (h *TestHandler) HeaderBinder1(c *gin.Context) {
+	userId := c.GetHeader("userId")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder1",
+		"userId": userId,
+	})
+}
+
+func (h *TestHandler) HeaderBinder2(c *gin.Context) {
+	header := header{}
+	c.BindHeader(&header)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder2",
+		"header": header,
+	})
+
+}
+
+func (h *TestHandler) QueryBinder1(c *gin.Context) {
+
+	id := c.Query("id")
+	name := c.Query("name")
+	family := c.Query("family")
+	fullName := name + " " + family
+	c.JSON(http.StatusOK, gin.H{
+		"result":    "HeaderBinder2",
+		"id":        id,
+		"full name": fullName,
+	})
+
+}
+
+func (h *TestHandler) QueryBinder2(c *gin.Context) {
+
+	id := c.QueryArray("id")
+	name := c.Query("name")
+	family := c.Query("family")
+	fullName := name + " " + family
+	c.JSON(http.StatusOK, gin.H{
+		"result":    "HeaderBinder2",
+		"ids":       id,
+		"full name": fullName,
+	})
+
+}
+
+func (h *TestHandler) ParamsBinder1(c *gin.Context) {
+
+	id := c.Param("id")
+	name := c.Param("name")
+	family := c.Param("family")
+	fullName := name + " " + family
+	c.JSON(http.StatusOK, gin.H{
+		"result":    "ParamsBinder1",
+		"ids":       id,
+		"full name": fullName,
+	})
+
+}
+
+func (h *TestHandler) BodyBinder(c *gin.Context) {
+
+	p := personData{}
+	c.ShouldBindJSON(&p)
+	fullName := p.Name + " " + p.Family
+
+	c.JSON(http.StatusOK, gin.H{
+		"result":    "BodyBinder",
+		"name":      p,
+		"full name": fullName,
+	})
+
+}
+
+func (h *TestHandler) FormBinder(c *gin.Context) {
+	p := personData{}
+	c.Bind(&p)
+	fullName := p.Name + " " + p.Family
+
+	c.JSON(http.StatusOK, gin.H{
+		"result":    "FormBinder",
+		"person":    p,
+		"full name": fullName,
+	})
+}
+
+func (h *TestHandler) FileBinder(c *gin.Context) {
+	file, _ := c.FormFile("file")
+	err := c.SaveUploadedFile(file, "file")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result":    "FileBinder",
+		"file Name": file.Filename,
+		"size":      file.Size,
+	})
+
+}
