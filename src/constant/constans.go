ff71fe077575422502a467b4add05d30d9a06c95
javadkavossi <jkavossi@gmail.com>
2024-11-22 14:00:00 +0100

    update

--- src/constant/constans.go
+++ src/constant/constans.go
@@ -9,12 +9,20 @@ const (
 
 	// Claims
 	AuthorizationHeaderKey string = "Authorization"
+
 	UserIdKey              string = "UserId"
+	
 	FirstNameKey           string = "FirstName"
+	
 	LastNameKey            string = "LastName"
+	
 	UsernameKey            string = "Username"
+	
 	EmailKey               string = "Email"
+	
 	MobileNumberKey        string = "MobileNumber"
+	
 	RolesKey               string = "Roles"
+	
 	ExpireTimeKey          string = "Exp"
 )
